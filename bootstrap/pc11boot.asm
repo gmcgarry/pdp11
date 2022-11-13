7264206ab147b69958e8cdaa53a48f9ba75db473
Gregory McGarry <g.mcgarry@ieee.org>
2022-11-13 23:13:42 +1100

    Add the absolute tape loader.

--- bootstrap/pc11boot.asm
+++ bootstrap/pc11boot.asm
@@ -1,18 +1,34 @@
-; ABSOLUTE BOOT LOADER
+; The high-speed paper tape reader and punch uses I/O
+; addresses 177550-177557.
+;
+; The Model 33 Teletype uses paper trap reader uses
+; I/O addresss 177560-177567.
+;
+; 0017744 is suitable for systems with 8K+ memory
+; 0037744 is suitable for systems with 16K+ memory
+; 0077744 is suitable for systems with 32K+ memory
+;
+; - will load 228B from the tape to 037400 ($3f00)
+; - generally the absolute loader will be loaded
+;   from the start of the tape which can be used
+;   to load larger files (with checksums)
+;
 
-; The high-speed paper tape reader and punch uses
-; I/O addresses 177550-177557.
+LOAD		= 037744
 
-; self-modifying code!
+KL11RCSR	=	177560
+PC11PRS		=	177550
 
-	.ORG	037744
-	MOV	reg,R1
-loop:	MOV	$352,R2
-	INC	*R1
-1:	TSTB	*R1
+	.ORG	LOAD
+
+	MOV	DEVICE,R1	; load device base address
+LOOP:	MOV	$352,R2	
+	INC	*R1		; set LSB of control register
+1:	TSTB	*R1		; wait for byte
 	BPL	1b
-	MOVB	2(R1),(loop+2-352)(R2)
-	INC	loop+2
-	BR	loop
-reg:	.WORD 177550
+	MOVB	2(R1),(LOOP+2-352)(R2)	; read byte and overwrite R2 load instruction
+	INC	LOOP+2
+	BR	LOOP
+DEVICE:	.WORD	PC11PRS
+
 	.END
